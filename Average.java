94b1027bd203481626552cf942708bdb3c14a407
KangheeKim <113897847+Rodingz@users.noreply.github.com>
2023-03-08 08:16:45 +0800

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,10 @@
+public class Average{
+    public static double average(double a,double b, double c, double d, double e){
+        return (a+b+c+d+e)/5;
+    }
+    public static void main(String[] args){
+        System.out.println(average(1.0, 2.0, 3.0, 4.0, 5.0));
+        System.out.println(average(6.0, 7.0, 8.0, 9.0, 10.0));
+        System.out.println(average(11.0, 12.0, 13.0, 14.0, 15.0));
+    }
+}
